8c65dd1f92bbfadf285ade0bcb6cb8f916d42fc3
snow-The <qq1346126834@gmail.com>
2021-10-20 23:20:31 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix=':Q ')
+bot = commands.Bot(command_prefix='Q:')
 
 @bot.event
 async def on_ready():
@@ -10,5 +10,9 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{bot.latency*1000}(ms)')
+
+@bot.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a + b)
 
 bot.run("")
