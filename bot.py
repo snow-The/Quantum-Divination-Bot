7e9b0c74c97c69e3267c4ccf250bd871fb6ba953
snow-The <qq1346126834@gmail.com>
2021-10-20 23:12:50 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix=':Q ')
 
 @bot.event
 async def on_ready():
     print(">> SQCS-D Bot is online! <<")
 
-bot.run("")
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{bot.latency*1000}(ms)')
+
+bot.run("OTAwMzUwNDUyMTMzMTM0MzU3.YXACsw.Xl45D1pcU1r8I4wasMVIcGdj3tg")
